0d2c4c1ee5477baaf65b4377e03405fbbe8da788
Octavio Cubillos <ocubillosj@gmail.com>
2017-10-25 12:11:44 -0300

    test 1

--- App_Ticket/admin.py
+++ App_Ticket/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Estado,Establecimiento,Ticket,Respuestas,Tecnico,Tema
 # Register your models here.
+admin.site.register(Estado)
+admin.site.register(Establecimiento)
+admin.site.register(Ticket)
+admin.site.register(Respuestas)
+admin.site.register(Tecnico)
+admin.site.register(Tema)
